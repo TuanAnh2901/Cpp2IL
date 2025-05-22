3ca28f9256ca8dca44f0ca5c76d35d8a2ab5149a
Imy <295238641@qq.com>
2025-05-22 19:47:51 +0800

    1

---
+++ Cpp2IL.Core/InstructionSets/Better/Flags/Processors/ArithmeticProcessor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Cpp2IL.Core.ISIL;
+using Disarm;
+
+namespace Cpp2IL.Core.InstructionSets.Better.Flags.Processors;
+
+public class ArithmeticProcessor : IFlagsProcessor
+{
+    public void GenerateCompareAndJump(IsilBuilder builder, FlagsState state, Arm64ConditionCode conditionCode, ulong branchTarget,
+        ulong addr)
+    {
+        switch (conditionCode)
+        {
+            case Arm64ConditionCode.NE: // 结果不等于0
+            {
+                // 处理不等于条件
+                builder.Compare(state.Address, state.Dest, InstructionSetIndependentOperand.MakeImmediate(0));
+                builder.JumpIfNotEqual(addr , branchTarget);
+                break;
+            }
+            case Arm64ConditionCode.PL:
+            {
+                // 处理大于等于条件
+                builder.Compare(state.Address, state.Dest, InstructionSetIndependentOperand.MakeImmediate(0));
+                builder.JumpIfGreaterOrEqual(addr , branchTarget);
+                break;
+            }
+            case Arm64ConditionCode.MI:
+            {
+               builder.Compare(state.Address, state.Dest, InstructionSetIndependentOperand.MakeImmediate(0));
+               builder.JumpIfLess( addr , branchTarget);
+                break;
+            }
+            default:
+                    throw   new Exception($"不支持的条件码 {conditionCode} 用于地址0x{addr:X} :   state "+state);
+        }
+    }
+
+    public void GenerateConditionalSelect(IsilBuilder builder, ulong addr, FlagsState state, InstructionSetIndependentOperand dest,
+        InstructionSetIndependentOperand trueValue, InstructionSetIndependentOperand falseValue,
+        Arm64ConditionCode conditionCode)
+    {
+        throw new System.NotImplementedException();
+    }
+}
